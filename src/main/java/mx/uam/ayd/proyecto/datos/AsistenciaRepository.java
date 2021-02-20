b6d50b3a5203a4ae290140eec9cb309a8b7e3b98
RMtzB <64701673+RMtzB@users.noreply.github.com>
2021-02-20 01:22:01 -0600

    HU-05 Terminada
    HU, pruebas unitarias correspondientes y documentacion terminada

--- src/main/java/mx/uam/ayd/proyecto/datos/AsistenciaRepository.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/AsistenciaRepository.java
@@ -4,17 +4,35 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-
 import mx.uam.ayd.proyecto.negocio.modelo.Asistencia;
 import mx.uam.ayd.proyecto.negocio.modelo.Empleado;
 
 /**
  * Repositorio para Asistencias
  * 
- * @author Cesar
+ * @author Raul Mb
+ * @since 15/02/2021
  */
 public interface AsistenciaRepository extends CrudRepository<Asistencia, Long> {
-	
+	/**
+	 * findByEmpleado:
+	 * Método encargado de obtener de la BD los registros de asistencia de un empleado
+	 * 
+	 * @param empleado empleado del cual se requiere obtener los registros
+	 * @return List<Asistencia> lista con todos lo registros encontrados (de tal empleado)
+	 */
 	public List<Asistencia> findByEmpleado(Empleado empleado);
-
+	
+	
+	
+	/**
+	 * findByEmpleadoAndFecha:
+	 * Método encargado de obtener de la BD el registro de asistencia de un empleado en
+	 * una fecha determinada 
+	 * 
+	 * @param empleado empleado del cual se requiere obtener el registro
+	 * @param fecha la fecha del registro que se requiere obtener
+	 * @return Asistencia la asistencia encontrada (de tal empleado)
+	 */
+	public Asistencia findByEmpleadoAndFecha(Empleado empleado, String fecha);
 }
